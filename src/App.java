c7e8a111e5c5799b4efeec4479bf04b7bcf531e0
benmccalmon <benmccalmon@gmail.com>
2022-11-23 10:43:44 -0500

    project folder setup

---
+++ src/App.java
@@ -0,0 +1,15 @@
+/*
+ * Author: Benjamin McCalmon
+ * Version: alpha-1.0
+ * Date last modified: 11/23/2022
+ * Bulls and Cows McCalmon implementation all rights reserved
+ */
+
+public class App {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
